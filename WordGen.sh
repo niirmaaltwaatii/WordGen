f087c5df11891cdf3333f31feeabb1d9c0a9e168
niirmaaltwaatii <twaatiiniirmaal@gmail.com>
2020-07-23 04:08:45 +0545

    WordGen.sh Created

---
+++ WordGen.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# WordGen
+# By Niirmaal Twaatii
+
+read -p "FirstName: " fn
+read -p "LastName: " ln
+read -p "Age: " ag
+read -p "Birth Year: " by
+read -p "Birth Month (eg.January): " bm
+read -p "Birth Day: " bd
+
+./wds.sh
+
+
